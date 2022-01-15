9193bb5528fb08e9cc4773956d9a0f8f32924408
fl-pereira <fpereira.media@gmail.com>
2022-01-15 12:10:23 -0300

    15 01 22 - praticando cálculos

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,10 @@ public class Main {
 		double preco2 	= 650.50;
 		double medida 	= 52.36515432;
 		
-		System.out.printf("Products:%n");
+		System.out.printf("Produtos:%n");
 		System.out.printf("%s, cujo o preço é R$ %.2f. %n", produto1, preco1);
 		System.out.printf("%s, cujo o preço é R$ %.2f. %n", produto2, preco2);
-		System.out.printf("Record: %d ans de idade, código %d e sexo: %s. %n", idade, cod, sexo);
+		System.out.printf("Record: %d anos de idade, código %d e sexo: %s. %n", idade, cod, sexo);
 		System.out.printf("Medida com 8 casas decimais: %f. %n", medida);
 		System.out.printf("Medida com 3 casas decimais: %.3f %n", medida);
 
